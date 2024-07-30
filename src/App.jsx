4a168dff37c4bda54937dd332c7d62a273a01ce3
Sheila Gonzalez <capacitaciones06.05@adviters.com>
2024-07-30 12:18:32 -0300

    react-router-dom funciona

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import { Link } from "react-router-dom";
-import NavBar from "./Components/NavBar/NavBar";
-import Login from "./Components/Login/Login";
-import Register from "./Components/Register/Register";
-import Footer from "./Components/Footer/Footer";
-import Aside from "./Aside/Aside";
+import { NavLink } from "react-router-dom";
+//import NavBar from "./Components/NavBar/NavBar";
+//import Login from "./Components/Login/Login";
+//import Register from "./Components/Register/Register";
+//import Footer from "./Components/Footer/Footer";
+//import Aside from "./Aside/Aside";
 
 function App() {
-  return <></>;
+  return ( <>
+    <NavLink to="admin">
+      <button className='my-button'>Go to AdminProfile</button>
+    </NavLink>
+    <NavLink to="movies">
+      <button className='my-button'>Go to Movies</button>
+    </NavLink>
+    <NavLink to="register">
+      <button className='my-button'>Go to Register</button>
+    </NavLink>
+    <NavLink to="login">
+      <button className='my-button'>Go to Login</button>
+    </NavLink>
+  </>)
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,49 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./Pages/Layout";
 import Movies from "./Components/Movies/Movies";
+import AdminProfile from "./Components/AdminProfile/AdminProfile";
+import Login from "./Components/Login/Login";
+import App from "./App";
+import Register from "./Pages/Register";
+import { PageNotFound } from "./Pages/PageNotFound/PageNotFound";
 
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="movies" element={<Movies />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-}
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <App />,
+      },
+      {
+        path: "admin",
+        element: <AdminProfile />,
+      },
+      {
+        path: "movies",
+        element: <Movies />,
+      },
+      {
+        path: "register",
+        element: <Register />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+    ]
+  },
+  {
+    path: "*",
+    element: <PageNotFound />,
+  }
+])
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
